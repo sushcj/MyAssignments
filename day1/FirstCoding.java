8e96297990f0d4bb1dcb066c45e10215023ec912
sushcj <146026162+sushcj@users.noreply.github.com>
2023-09-25 11:57:39 -0700

    Add files via upload
    day 1 project

---
+++ day1/FirstCoding.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class FirstCoding {
+	
+// main method is the entry point of execution
+	
+	public static void main(String[] args) {
+		
+	// type syso and then press ctl+space together
+		
+	 System.out.println("Hello Testleaf");
+	 System.out.println("Hi Susmitha");
+
+	}
+
+}
